775f6c6c7545b47f28f4435dc02596a20e9aa91c
SawThandar <sawthandar@better.hr>
2024-05-31 11:16:04 +0630

    update kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    id("com.android.application").version("8.0.2").apply(false)
-    id("com.android.library").version("8.0.2").apply(false)
-    kotlin("android").version("1.8.21").apply(false)
-    kotlin("multiplatform").version("1.8.21").apply(false)
+    id("com.android.application").version("8.2.1").apply(false)
+    id("com.android.library").version("8.2.1").apply(false)
+    kotlin("android").version("1.9.22").apply(false)
+    kotlin("multiplatform").version("1.9.22").apply(false)
     id("org.jetbrains.compose").version("1.5.12").apply(false)
     id("com.apollographql.apollo3") version "3.8.2" apply false
 }
