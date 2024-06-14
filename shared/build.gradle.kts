b98349e01aaef716bd09ff73a54915f9ec83a9d0
SawThanDar18 <sawthandar1998.pw@gmail.com>
2024-06-14 16:17:05 +0630

    adding coroutines library for ios

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -121,6 +121,7 @@ kotlin {
         }
         val iosMain by getting {
             dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
                 implementation("io.insert-koin:koin-core:3.1.2")
                 implementation(KTOR.clientiOS)
                 implementation(KTOR.clientDarwin)
